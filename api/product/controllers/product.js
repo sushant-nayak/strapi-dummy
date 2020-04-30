5e335e0743059eb69607042ef8ddca278cebcadc
Sushant Nayak <sushant.nayak@live.com>
2020-04-29 22:13:56 -0400

    Initial commit

---
+++ api/product/controllers/product.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    // async find(ctx) {
+    //     const products = await strapi.services.product.find(ctx.query)
+
+    //     return products.map(product => {
+    //         return product.title;
+    //     })
+    // }
+};
